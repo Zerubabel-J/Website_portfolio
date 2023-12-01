a8f91d768e8db3d5b0dadafd280241fe75ae89d8
Zerubabel Jember <zeruspeaks@gmail.com>
2023-12-01 13:06:20 +0300

    bootstrap is imported

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return <h2>Portfolio-project......</h2>;
 }
