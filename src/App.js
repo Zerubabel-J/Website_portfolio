cd8cb21b84040a56bf82d52e30014a4882df281f
Zerubabel <zerucodes@gmail.com>
2023-12-01 13:11:47 +0300

    react fragment is used

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return <h2>Portfolio-project......</h2>;
+  return (
+    <>
+      <h2>Portfolio-project......</h2>
+      <h3>How you doing Zerubabel</h3>
+    </>
+  );
 }
 
 export default App;
